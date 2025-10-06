684aea2bd40f576f6eaa128a9e53ac6d6bac5128
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-10-06 11:46:09 +0900

    chore: add base package structure

--- src/main/java/com/loadtest/LoadTestApplication.java
+++ src/main/java/com/loadtest/LoadTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpaceKeepApplication {
+public class LoadTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpaceKeepApplication.class, args);
+		SpringApplication.run(LoadTestApplication.class, args);
 	}
 
 }
