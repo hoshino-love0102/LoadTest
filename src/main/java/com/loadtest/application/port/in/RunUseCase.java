4730e589207febe7efebfc2b1d32d55a9c1a75ef
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-10-10 10:58:31 +0900

    feat(usecase): expose time-series metrics retrieval

--- src/main/java/com/loadtest/application/port/in/RunUseCase.java
+++ src/main/java/com/loadtest/application/port/in/RunUseCase.java
@@ -2,7 +2,9 @@ package com.loadtest.application.port.in;
 
 import com.loadtest.domain.model.TestRun;
 import com.loadtest.domain.model.TestReport;
+import com.loadtest.domain.model.RunSample;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RunUseCase {
@@ -10,4 +12,5 @@ public interface RunUseCase {
     TestRun getStatus(UUID runId);
     void stop(UUID runId);
     TestReport getReport(UUID runId);
+    List<RunSample> getTimeSeries(UUID runId);
 }
